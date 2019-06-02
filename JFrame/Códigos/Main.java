53b8c5782a35aabb95e1e6ce490fc4ffd4307c01
Jonas Nascimento <jonashnascimento@gmail.com>
2019-06-02 20:11:08 -0300

    JFrame
    Interfaces gráficas em java

---
+++ JFrame/Códigos/Main.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        loginFrame loginf = new loginFrame();
+    }
+}
