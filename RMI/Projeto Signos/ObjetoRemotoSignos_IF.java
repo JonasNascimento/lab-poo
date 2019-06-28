74e42766661bb82673fbbfe4f56c902710b1a0da
Jonas Nascimento <jonashnascimento@gmail.com>
2019-06-27 21:19:37 -0300

    Excluido

--- RMI/Projeto Signos/ObjetoRemotoSignos_IF.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ObjetoRemotoSignos_IF extends Remote{
-    String getMensagemSigno(String umSigno) throws RemoteException;
-}
